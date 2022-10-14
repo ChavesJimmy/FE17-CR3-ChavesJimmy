fe79d83433dc59e70efadd3d07348a63059a30cb
Jimmy CHAVES <jimmybakerchaves@gmail.com>
2022-10-14 10:36:01 +0200

    added routing path + navbar + footer

--- CR3Jimmy/src/app/app-routing.module.ts
+++ CR3Jimmy/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {
+  path : "", component:HomeComponent
+ } ,
+ {
+  path:"about", component:AboutComponent
+ }, 
+ {
+  path:"menu" , component:MenuComponent
+ },
+ {
+  path:"cart" , component:CartComponent
+ }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
